d8cc1b02b3217f310714cb0be241758de1ac5d7a
ngochatake <ngochatake22@gmail.com>
2022-01-26 15:47:30 +0700

    Finish Weather App

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,30 +1,13 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?appid=ff8d52393cc1b950360d8f3b231792f5&q=Danang&units=metric";
-
-    https.get(url, function(response) {
-        console.log(response.statusCode);
-
-        response.on("data", function(data) {
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description;
-            const imageURL = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
-            
-            res.setHeader("Content-type", "text/html");
-            res.write("The weather is currently " + weatherDescription);
-            res.write("<h1> The temperature in Danang is " + temp + " degress Celcius.</h1>");
-            res.write("<img src="+ imageURL + ">");
-            res.send();
-        });
-
-    });
    
+    res.sendFile(__dirname + "/index.html");
 
     // JSON stringify >< JSON parse
     var server = {
@@ -38,7 +21,29 @@ app.get("/", function(req, res) {
     //res.send("Server is up and running");
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
+    
+    const apiKey = "ff8d52393cc1b950360d8f3b231792f5";
+    const unit = "metric";
+    const query = req.body.cityName;
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + query +"&units=" + unit;
+
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const imageURL = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
+
+            res.setHeader("Content-type", "text/html");
+            res.write("The weather is currently " + weatherDescription);
+            res.write("<h1> The temperature in " + query  + " is " + temp + " degress Celcius.</h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
+        });
+    });
 
 });
 
