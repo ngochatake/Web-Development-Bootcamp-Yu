7eff5df9bdfa843758dbe4f6f09da173f4692d71
ngochatake <ngochatake22@gmail.com>
2022-01-30 22:33:15 +0700

    Newsletter first commit

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const API_KEY = process.env.APP_API_KEY;
+const AUDIENCE_ID = process.env.APP_AUDIENCE_ID;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+    const fName = req.body.fName;
+    const lName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fName,
+                    LNAME: lName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us20.api.mailchimp.com/3.0/lists/" + AUDIENCE_ID;
+
+    const options = {
+        method: "POST",
+        auth: "ngocpt:" + API_KEY   
+    };
+
+    const req_submit = https.request(url, options, function (response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    req_submit.write(jsonData);
+    console.log();
+    req_submit.end();
+});
+
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+});
+
+// API Key
+//3c586acf8ad619a8f64e40631c996ea3-us20
+
+//List ID == Audience ID
+//6d40bf5fdf
